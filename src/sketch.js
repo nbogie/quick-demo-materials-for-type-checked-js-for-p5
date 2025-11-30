0342dd0a4a97f65959539b26dab1e149c9cdc604
nbogie <neillbogie@googlemail.com>
2025-11-30 11:46:40 +0000

    add some simple errors to first sketch

--- src/sketch.js
+++ src/sketch.js
@@ -4,7 +4,15 @@ function setup() {
 }
 
 function draw() {
-    circle(mouseX, mouseY, 50);
+    circle(mouseX, mouseY, 10);
+}
 
-    const v = p5.Vector.fromAngle(0, 100);
+function mousePressed() {
+    rectMode(SQAURE);
+    square(width / 2, hieght / 2, 100);
+}
+
+function mouseDragged() {
+    fill(random(255), random(255), random(255), random(255), 100);
+    circle(mouseX, mouseY, random(50, 100));
 }
